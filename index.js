b5f9ce3f94c3f7020ab59d3920ba961f35922e3f
Nimendra Pethiyagoda <nimendra@stridesolutions.com.au>
2021-03-21 14:29:57 +1100

    simple node app with test

---
+++ index.js
@@ -0,0 +1,14 @@
+//importing node framework
+var express = require('express');
+let port = 3000;
+ 
+var app = express();
+//Respond with "hello world" for requests that hit our root "/"
+app.get('/', function (req, res) {
+ res.send('hello world');
+});
+//listen to port 3000 by default
+app.listen(process.env.PORT || port);
+
+console.info(`nodeJS App Started on ${port}`)
+module.exports = app;
