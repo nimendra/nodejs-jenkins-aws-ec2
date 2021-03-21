fd197514b7bcff1cf6cb5df4ad94c3b1f8591c29
Nimendra Pethiyagoda <nimendra@stridesolutions.com.au>
2021-03-21 16:08:34 +1100

    v0.2

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let port = 3000;
 var app = express();
 //Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('hello world');
+ res.send('hello world, updated and deployed through Jenkins!');
 });
 //listen to port 3000 by default
 app.listen(process.env.PORT || port);
